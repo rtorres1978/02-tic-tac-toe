a1a85c0e7e66e8e1efc2320eb9d9e6fcdfa6771c
rtorres1978 <com-sic@hotmail.com>
2024-12-24 13:24:02 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,68 @@
+import { useState } from "react";
+import "./App.css";
+import confetti from 'canvas-confetti';
+import {Square} from '../src/components/Square.jsx'
+import {TURNS} from '../src/constants.js'
+import {checkWinnerFrom, checkEndGame } from '../src/login/board.js'
+import {WinnerModal} from '../src/components/WinnerModal'
+function App() {
+  const [board, setBoard] = useState(Array(9).fill(null));
+  const [turn, setTurn] = useState(TURNS.X);
+  const [winner, setWinner] = useState(null);
+
+
+
+
+
+  const resetGame = () => {
+    setBoard(Array(9).fill(null));
+    setTurn(TURNS.X);
+    setWinner(null);
+  };
+
+  const updateBoard = (index) => {
+    if (board[index] || winner) return; // Evitar movimientos si ya hay ganador
+
+    const newBoard = [...board];
+    newBoard[index] = turn;
+    setBoard(newBoard);
+
+    const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X;
+    setTurn(newTurn);
+
+    const newWinner = checkWinnerFrom(newBoard);
+    if (newWinner) {
+      confetti()
+      setWinner(newWinner);
+    } else if (checkEndGame(newBoard)) {
+      setWinner(false); // Indicar empate
+    }
+  };
+
+  return (
+    <main className="board">
+      <h1>Tres en Raya</h1>
+      <button onClick={resetGame}>Reset del Juego</button>
+      <section className="game">
+        {board.map((value, index) => {
+          return (
+            <Square key={index} index={index} updateBoard={updateBoard}>
+              {value}
+            </Square>
+          );
+        })}
+      </section>
+
+      <section className="turn">
+        <Square isSelected={turn === TURNS.X}>{TURNS.X}</Square>
+        <Square isSelected={turn === TURNS.O}>{TURNS.O}</Square>
+      </section>
+
+     <WinnerModal resetGame={resetGame} winner={winner}/>
+    </main>
+  );
+}
+
+export default App;
+
+//npm install canvas-confetti -E
